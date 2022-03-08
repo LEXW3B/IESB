5c7227743e61dd2c1548dc8d8b4c31e56026f6f0
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-03-08 16:24:35 -0300

    jmgfd

---
+++ PYTHON/POO-python/ggfdsg.py
@@ -0,0 +1,12 @@
+
+
+alturaMetros=[1.65,1.80,1.72,1.74,1.68,1.91]
+
+def main( ):
+    print filter(lambda x: x > 1.70 > 1.90 ,alturaMetros) 
+
+    return 0
+if alturaMetros == ' main ':
+    main( )
+
+
